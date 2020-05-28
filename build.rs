ed2f2b23158aed0f620f29d08a5b00def218ab28
Jef <jackefransham@gmail.com>
2020-05-28 07:24:07 +0200

    Replace custom-written bindings with bindgen-generated ones

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+extern crate bindgen;
 extern crate cmake;
 extern crate pkg_config;
 
@@ -5,11 +6,6 @@ use cmake::Config;
 use std::env;
 
 fn main() {
-    // Use system libassimp if it exists
-    if let Ok(..) = pkg_config::Config::new().atleast_version("4.0.0").find("assimp") {
-        return
-    }
-
     // Compile assimp from source
     // Disable unnecessary stuff, it takes long enough to compile already
     let dst = Config::new("assimp")
@@ -19,16 +15,62 @@ fn main() {
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("CMAKE_SUPPRESS_DEVELOPER_WARNINGS", "ON")
         .define("LIBRARY_SUFFIX", "")
+        .define("CMAKE_C_COMPILER", "clang")
+        .define("CMAKE_C_FLAGS", "-fPIC")
         .build();
-    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").display()
+    );
 
     // Link to assimp and its dependencies
-    let debug_postfix = if env::var("DEBUG").unwrap() == "true" { "d" } else { "" };
+    let debug_postfix = if env::var("DEBUG").unwrap() == "true" {
+        "d"
+    } else {
+        ""
+    };
     println!("cargo:rustc-link-lib=static=assimp{}", debug_postfix);
     println!("cargo:rustc-link-lib=static=IrrXML{}", debug_postfix);
-    if !pkg_config::find_library("zlib").is_ok() {
-        println!("cargo:rustc-link-lib=static=zlibstatic{}", debug_postfix);
+
+    let minizip = pkg_config::probe_library("minizip").unwrap();
+    for path in minizip.link_paths {
+        println!("cargo:rustc-link-path={}", path.to_str().unwrap());
     }
+    for lib in minizip.libs {
+        println!("cargo:rustc-link-lib={}", lib);
+    }
+
+    let zlib = pkg_config::probe_library("zlib").unwrap();
+    for path in zlib.link_paths {
+        println!("cargo:rustc-link-path={}", path.to_str().unwrap());
+    }
+    for lib in zlib.libs {
+        println!("cargo:rustc-link-lib={}", lib);
+    }
+
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_args(&["-F", "assimp/include", "-F", "assimp/contrib/irrXML"])
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .blacklist_item("FP_ZERO")
+        .blacklist_item("FP_SUBNORMAL")
+        .blacklist_item("FP_NORMAL")
+        .blacklist_item("FP_NAN")
+        .blacklist_item("FP_INFINITE")
+        .derive_partialeq(true)
+        .derive_eq(true)
+        .derive_hash(true)
+        .derive_debug(true)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = std::path::PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let bindings_path = out_path.join("bindings.rs");
+    bindings
+        .write_to_file(&bindings_path)
+        .unwrap_or_else(|e| panic!("Couldn't write to {:?}: {:?}", bindings_path, e));
 
     // Link to libstdc++ on GNU
     let target = env::var("TARGET").unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,6 @@
-#[macro_use]
-extern crate bitflags;
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(improper_ctypes)]
 
-pub use anim::*;
-pub use camera::*;
-pub use cexport::*;
-pub use cfileio::*;
-pub use cimport::*;
-pub use importerdesc::*;
-pub use light::*;
-pub use material::*;
-pub use mesh::*;
-pub use metadata::*;
-pub use postprocess::*;
-pub use scene::*;
-pub use texture::*;
-pub use types::*;
-pub use version::*;
-
-mod anim;
-mod camera;
-mod cexport;
-mod cfileio;
-mod cimport;
-pub mod config;
-mod importerdesc;
-mod light;
-mod material;
-mod mesh;
-mod metadata;
-mod postprocess;
-mod scene;
-mod texture;
-mod types;
-mod version;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
